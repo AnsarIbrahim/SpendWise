1ff97bbd53da3fe34cb4a46984b51c41485b7e80
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-11-24 19:33:14 +0530

    Add User Inputs

---
+++ components/ManageExpense/styles.js
@@ -0,0 +1,66 @@
+import { StyleSheet } from "react-native";
+import { GlobalStyles } from "../../constants/styles";
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginVertical: 8,
+    marginHorizontal: 4,
+  },
+  label: {
+    fontSize: 12,
+    color: GlobalStyles.colors.primary100,
+    marginBottom: 4,
+  },
+  input: {
+    backgroundColor: GlobalStyles.colors.primary100,
+    padding: 6,
+    borderRadius: 6,
+    fontSize: 18,
+    color: GlobalStyles.colors.primary700,
+  },
+  inputMultiline: {
+    minHeight: 100,
+    textAlignVertical: "top",
+  },
+  form: {
+    marginTop: 80,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "white",
+    marginVertical: 24,
+    textAlign: "center",
+  },
+  inputsRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  rowInput: {
+    flex: 1,
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
+  },
+  error: {
+    color: GlobalStyles.colors.error500,
+    fontSize: 14,
+    marginHorizontal: 4,
+    textAlign: "center",
+    margin: 8,
+  },
+  invalidLabel: {
+    color: GlobalStyles.colors.error500,
+  },
+  invalidInput: {
+    backgroundColor: GlobalStyles.colors.error50,
+  },
+});
+
+export default styles;
